66921915eb4c1f749676d0c392f1b77fe5916bf1
gabonada1 <2301101234@student.buksu.edu.ph>
2026-04-26 16:24:04 +0800

    Call Vite through root Windows shim

--- vite.cmd
+++ vite.cmd
@@ -1,18 +1,36 @@
 @echo off
 setlocal
 
+for /f "delims=" %%I in ('where node.exe 2^>nul') do (
+    set "NODE_EXE=%%I"
+    goto :found_node
+)
+
+if exist "%ProgramFiles%\nodejs\node.exe" set "NODE_EXE=%ProgramFiles%\nodejs\node.exe"
+if not defined NODE_EXE if exist "%ProgramFiles(x86)%\nodejs\node.exe" set "NODE_EXE=%ProgramFiles(x86)%\nodejs\node.exe"
+if not defined NODE_EXE if exist "%LocalAppData%\Programs\nodejs\node.exe" set "NODE_EXE=%LocalAppData%\Programs\nodejs\node.exe"
+
+:found_node
+if not defined NODE_EXE (
+    echo node.exe could not be found. Install Node.js or add node.exe to PATH.
+    exit /b 1
+)
+
 for /f "delims=" %%I in ('where npm.cmd 2^>nul') do (
-    set "NPM_DIR=%%~dpI"
+    set "NPM_CMD=%%I"
     goto :found_npm
 )
 
 :found_npm
-if defined NPM_DIR set "PATH=%NPM_DIR%;%PATH%"
+if not defined NPM_CMD (
+    echo npm.cmd could not be found. Install Node.js or add npm.cmd to PATH.
+    exit /b 1
+)
 
-if not exist "%~dp0node_modules\.bin\vite.cmd" (
-    call npm install --production=false
+if not exist "%~dp0node_modules\vite\bin\vite.js" (
+    call "%NPM_CMD%" install --production=false
     if errorlevel 1 exit /b %errorlevel%
 )
 
-call "%~dp0node_modules\.bin\vite.cmd" %*
+call "%NODE_EXE%" "%~dp0node_modules\vite\bin\vite.js" %*
 exit /b %errorlevel%
